8de62f834ee2b265790e45f8accf47c089caa98a
acdweb <github@acd.co.jp>
2011-04-24 22:38:21 +0900

    flash

--- top.js
+++ top.js
@@ -1,3 +1,7 @@
+var MM_contentVersion = 6;
+	var MM_PluginVersion = FlashPlayerVer();
+	var MM_FlashCanPlay = MM_PluginVersion >= MM_contentVersion;
+if ( MM_FlashCanPlay ) {
 		document.write('<OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"');
 	document.write('  codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" ');
 	document.write(' ID="top" WIDTH="550" HEIGHT="330" ALIGN="">');
@@ -9,4 +13,26 @@
 	document.write(' </OBJECT>');
 } else{
 	document.write('<IMG SRC="img/acd_img.jpg" WIDTH="550" HEIGHT="330" usemap="#top" BORDER=0>');
+}
+
+function FlashPlayerVer(){
+//初期設定
+var flashplayer_ver = 0;
+//IE以外の場合
+if(navigator.plugins && navigator.mimeTypes['application/x-shockwave-flash']){
+var plugin = navigator.mimeTypes["application/x-shockwave-flash"].enabledPlugin;
+//Flash Playerがインストールされている場合
+if(plugin){flashplayer_ver = parseInt(plugin.description.match(/\d+\.\d+/));}
+}
+//IEの場合、もしくはFlash Playerがインストールされていない場合
+else{
+//IEでFlash Playerがインストールされている場合
+try{
+var flashOCX = new ActiveXObject("ShockwaveFlash.ShockwaveFlash").GetVariable("$version").match(/([0-9]+)/);
+if(flashOCX){flashplayer_ver = parseInt(flashOCX[0]);}
+}catch(e){}
+}
+//Flash Playerがインストールされていない、もしくはバージョンが6以下の場合
+if(flashplayer_ver <= 6){flashplayer_ver = 0;}
+return flashplayer_ver;
 }
